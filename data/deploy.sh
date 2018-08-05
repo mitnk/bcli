9551a8aa244574840538e0b715e57236a87d2114
Hugo Wang <w@mitnk.com>
2018-08-05 23:57:38 +0800

    remove ad-hoc files

--- data/deploy.sh
+++
@@ -1,7 +0,0 @@
-dir_data="$HOME/gethDataDir"
-file_genesis="${dir_data}/genesis.json"
-file_logs="${dir_data}/geth.log"
-
-geth --datadir "$dir_data" init "$file_genesis"
-
-nohup geth --datadir "$dir_data" --networkid 917 2>"$file_logs" &
